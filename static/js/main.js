dd2f95b676f2bdb9e4b5e73ef23f427c522131c6
SerhiiRudyi <srudyjj@gmail.com>
2018-03-31 21:16:50 +0300

    Add bay animation

--- static/js/main.js
+++ static/js/main.js
@@ -1,9 +1,34 @@
 $(function() {
-    var top = $(window).scrollTop();
-    console.log(top);
+
+    var a;
+    var b;  
+    var c;
+    var d;
+    var form;
+    var basket = $(".basket");
     $(".bay").click(function () {
-        $("body, html").animate(
-        {scrollTop: 0}, 1000);
+       
+        form = $(this)
+          .parent()
+          .parent();
+        a = form.find("img").attr("src");
+        console.log(a);
+        b = $("<img>", {src: a});
+        a = form.find("img");
+        c = a.offset().top - $(window).scrollTop();
+        d = a.offset().left;
+
+        b.css({position: "fixed", top:c, left:d})
+        .prependTo(form);
+        basketLeft = basket.offset().left;          
+        b.animate({top: 0, left: basketLeft, width: 65, height: 55, opacity: "0.5"}, 5000);
+        $("body, html").animate({scrollTop: 0}, 4500);
+
+        function rm(what) {
+           return what.remove();
+        };
+        setTimeout(rm, 4500, b);
+        
         return false;
     });    
 });
